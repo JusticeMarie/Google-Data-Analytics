33caedfbfa70f134f016df3c0f756cb505f574ec
JusticeMarie <justicemarie55@gmail.com>
2024-04-24 12:19:33 -0500

    Update Wiki Larger dataset.sql

--- Beginner Projects/Wiki Larger dataset.sql
+++ Beginner Projects/Wiki Larger dataset.sql
@@ -1,3 +1,5 @@
+-- This query sorts and filters data from the dataset bigquery-samples.wikipedia_benchmark.Wiki10B, 
+-- which is a sample from the Wikipedia public dataset that contains 10 billion rows.
 SELECT
   language,
   title,
