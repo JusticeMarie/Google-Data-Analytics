8815db6ecc22a5cae2b3319a77aa10c3218c1388
JusticeMarie <justicemarie55@gmail.com>
2024-04-27 00:29:55 -0500

    Create Zillow Days On Market.sql

---
+++ Beginner Projects/Zillow Days On Market.sql
@@ -0,0 +1,8 @@
+-- This query sorts and filters data from Zillow to see the median amount of days a house is on the market
+SELECT 
+  _2024_03_31
+
+ FROM `justice-project-419018.zillow_sales.zillow_market` 
+ 
+ WHERE
+  StateName = 'TX' AND RegionName = 'San Antonio, TX'
